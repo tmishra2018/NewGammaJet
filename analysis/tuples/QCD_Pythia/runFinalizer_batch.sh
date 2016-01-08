e8c41da31b0699b6d4cb1ef832a75ee695485473
FPreiato <federico.preiato@roma1.infn.it>
2016-01-08 18:27:31 +0100

    To finalize

---
+++ analysis/tuples/QCD_Pythia/runFinalizer_batch.sh
@@ -0,0 +1,5 @@
+bsub -q cmslong -o logfile_alphacut010.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Pythia/QCD_alphacut010.src
+bsub -q cmslong -o logfile_alphacut015.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Pythia/QCD_alphacut015.src
+bsub -q cmslong -o logfile_alphacut020.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Pythia/QCD_alphacut020.src
+bsub -q cmslong -o logfile_alphacut025.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Pythia/QCD_alphacut025.src
+bsub -q cmslong -o logfile_alphacut030.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Pythia/QCD_alphacut030.src
