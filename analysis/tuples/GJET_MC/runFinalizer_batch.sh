83c1bf2888baf5368bd6d97e4764ccb5552b1818
FPreiato <federico.preiato@roma1.infn.it>
2016-01-08 18:21:12 +0100

    Re-name directory

--- analysis/tuples/GJET_MC/runFinalizer_batch.sh
+++
@@ -1,5 +0,0 @@
-bsub -q cmslong -o logfile_alphacut010.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut010.src 
-bsub -q cmslong -o logfile_alphacut015.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut015.src
-bsub -q cmslong -o logfile_alphacut020.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut020.src
-bsub -q cmslong -o logfile_alphacut025.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut025.src
-bsub -q cmslong -o logfile_alphacut030.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut030.src
--- analysis/tuples/GJET_MC/runFinalizer_batch_partial.sh
+++
@@ -1,1 +0,0 @@
-bsub -q cmslong -o logfile_alphacut030_bis.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_MC/GJet_alphacut030.src
