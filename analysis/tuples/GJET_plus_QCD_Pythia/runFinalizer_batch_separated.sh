828901afe0eede4223ba16c0d9db84e4c10a6308
FPreiato <federico.preiato@roma1.infn.it>
2016-04-22 12:23:02 +0200

    Deleted scripts to run in batch

--- analysis/tuples/GJET_plus_QCD_Pythia/runFinalizer_batch_separated.sh
+++
@@ -1,2 +0,0 @@
-bsub -q cmslong -o logfile_GJet_alphacut030.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_plus_QCD_Pythia/GJet_alphacut030.src 
-bsub -q cmslong -o logfile_QCD_alphacut030.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/GJET_plus_QCD_Pythia/QCD_alphacut030.src 
