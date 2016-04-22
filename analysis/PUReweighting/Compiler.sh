c841899f24cb7e02e8525672ba13b3459ce6e06d
FPreiato <federico.preiato@roma1.infn.it>
2016-04-22 17:55:08 +0200

    Create a Makefile to compile

--- analysis/PUReweighting/Compiler.sh
+++
@@ -1,8 +0,0 @@
-echo Compilo PUWeight_NvtxBased.c
-g++ -o PUWeight_NvtxBased.exe PUWeight_NvtxBased.c `root-config --cflags  --glibs`
-echo Compilo generate_mc_pileup.c
-g++ -o generate_mc_pileup.exe generate_mc_pileup.c `root-config --cflags  --glibs`
-echo Compilo Ratio_Test.c
-g++ -o Ratio_Test.exe Ratio_Test.c `root-config --cflags  --glibs`
-
-rm *~
