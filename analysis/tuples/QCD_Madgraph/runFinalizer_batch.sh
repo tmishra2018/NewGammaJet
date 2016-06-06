c99be3c7724c96c77b939b6618dd2d70c863764a
FPreiato <federico.preiato@roma1.infn.it>
2016-06-06 15:29:58 +0200

    Deleted script to run madgraph

--- analysis/tuples/QCD_Madgraph/runFinalizer_batch.sh
+++
@@ -1,5 +0,0 @@
-bsub -q cmslong -o logfile_alphacut010.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Madgraph/QCD_alphacut010.src
-bsub -q cmslong -o logfile_alphacut015.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Madgraph/QCD_alphacut015.src
-bsub -q cmslong -o logfile_alphacut020.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Madgraph/QCD_alphacut020.src
-bsub -q cmslong -o logfile_alphacut025.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Madgraph/QCD_alphacut025.src
-bsub -q cmslong -o logfile_alphacut030.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/tuples/QCD_Madgraph/QCD_alphacut030.src
