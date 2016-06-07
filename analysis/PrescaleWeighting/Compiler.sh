2a637424c3e7d978a028c4c50057cc44f20331f5
FPreiato <federico.preiato@roma1.infn.it>
2016-06-07 10:05:54 +0200

    Deleted or Moved Old stuff --- cleaning operation

--- analysis/PrescaleWeighting/Compiler.sh
+++
@@ -1,5 +0,0 @@
-echo Compilo MC_ptPhot_scaled.c
-g++ -o MC_ptPhot_scaled.exe MC_ptPhot_scaled.c `root-config --cflags  --glibs`
-echo Compilo PrescaleWeight_After.c
-g++ -o PrescaleWeight_After.exe PrescaleWeight_After.c `root-config --cflags  --glibs`
-rm *~
--- analysis/PrescaleWeighting/run_batch.sh
+++
@@ -1,1 +0,0 @@
-bsub -q cmslong -o logfile.log source /cmshome/fpreiato/GammaJet/CMSSW_7_4_14/src/JetMETCorrections/GammaJetFilter/analysis/PrescaleWeighting/config_batch.src 
