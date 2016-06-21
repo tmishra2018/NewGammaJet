3adda53bbad919909495dda5630e3e08dc8094c3
FPreiato <federico.preiato@roma1.infn.it>
2016-06-21 12:50:18 +0200

    Cleaning old/useless code

--- analysis/draw/runDrawer_batch.sh
+++
@@ -1,7 +0,0 @@
-bsub -q cmslong -o AlphaCut010/logfile.log source /cmshome/fpreiato/GammaJet/CMSSW_7_6_3_patch2/src/JetMETCorrections/GammaJetFilter/analysis/draw/Plot/2016-01-19_ReReco76X/AlphaCut010/drawer.src
-bsub -q cmslong -o AlphaCut015/logfile.log source /cmshome/fpreiato/GammaJet/CMSSW_7_6_3_patch2/src/JetMETCorrections/GammaJetFilter/analysis/draw/Plot/2016-01-19_ReReco76X/AlphaCut015/drawer.src
-bsub -q cmslong -o AlphaCut020/logfile.log source /cmshome/fpreiato/GammaJet/CMSSW_7_6_3_patch2/src/JetMETCorrections/GammaJetFilter/analysis/draw/Plot/2016-01-19_ReReco76X/AlphaCut020/drawer.src
-bsub -q cmslong -o AlphaCut030/logfile.log source /cmshome/fpreiato/GammaJet/CMSSW_7_6_3_patch2/src/JetMETCorrections/GammaJetFilter/analysis/draw/Plot/2016-01-19_ReReco76X/AlphaCut030/drawer.src
-
-
-
