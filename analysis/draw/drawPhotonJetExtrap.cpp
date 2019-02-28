485c0c1b5715d1e04e95e156d6ac0f19583e3318
lucastorterotot <lucas.torterotot@gmail.com>
2019-02-28 13:16:53 +0100

    Corrections on binning for plots.

--- analysis/draw/drawPhotonJetExtrap.cpp
+++ analysis/draw/drawPhotonJetExtrap.cpp
@@ -123,7 +123,7 @@ int main(int argc, char* argv[]) {
     db->set_FIXM(FIXM);
     db->set_EXCLUDEFIRSTPOINT(EXCLUDE_FIRST_POINT);
 
-    TH1D *ptPhot = (TH1D*)dataFile->Get("analysis/ptPhoton_passedID");
+    TH1D *ptPhot = (TH1D*)dataFile->Get("analysis/ptPhoton");
     PtBinning ptBinning;
     size_t ptBinningSize = ptBinning.size();
     std::vector<std::pair<float, float> > ptBins = ptBinning.getBinning();
