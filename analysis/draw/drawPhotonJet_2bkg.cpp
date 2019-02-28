d749a173a7653f59b65790285e4cd134d7ff649b
lucastorterotot <lucas.torterotot@gmail.com>
2019-02-28 11:15:52 +0100

    Corrections on binning for plots.

--- analysis/draw/drawPhotonJet_2bkg.cpp
+++ analysis/draw/drawPhotonJet_2bkg.cpp
@@ -175,7 +175,7 @@ int main(int argc, char* argv[]) {
  // db->set_rebin(5);
   db->setOutputGraphs(OUTPUT_GRAPHS);
 
-  TH1D *ptPhot = (TH1D*)dataFile->Get("analysis/ptPhoton_passedID");
+  TH1D *ptPhot = (TH1D*)dataFile->Get("analysis/ptPhoton");
   PtBinning ptBinning;
   size_t ptBinningSize = ptBinning.size();
   std::vector<std::pair<float, float> > ptBins = ptBinning.getBinning();
