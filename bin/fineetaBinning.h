bc1f5b2e18696a4afe2ac5985625f5008db59174
lucastorterotot <lucas.torterotot@gmail.com>
2019-03-07 14:20:20 +0100

    Fine eta binning update for Henning Kirschenmann

--- bin/fineetaBinning.h
+++ bin/fineetaBinning.h
@@ -74,52 +74,94 @@ class fineEtaBinning {
 
       
       
-      bin.bin = std::make_pair(0., 0.522);
-      bin.name = "eta0005";
-      bin.title = "|#eta| < 0.52";  
+      bin.bin = std::make_pair(0., 0.261);
+      bin.name = "eta0003";
+      bin.title = "|#eta| < 0.261";  
       mEtaBins.push_back(bin);
       
+      bin.bin = std::make_pair(0.261, 0.522);
+      bin.name = "eta0305";
+      bin.title = "0.261 #leq |#eta| < 0.522";  
+      mEtaBins.push_back(bin);
       
       bin.bin = std::make_pair(0.522,0.783);
       bin.name = "eta0508";
-      bin.title = "0.5 #leq |#eta| < 0.8";
+      bin.title = "0.522 #leq |#eta| < 0.783";
       mEtaBins.push_back(bin);
 
-      bin.bin = std::make_pair(0.783,1.131);
-      bin.name = "eta0811";
-      bin.title = "0.8 #leq |#eta| < 1.1";
+      bin.bin = std::make_pair(0.783,1.044);
+      bin.name = "eta0810";
+      bin.title = "0.783 #leq |#eta| < 1.044";
       mEtaBins.push_back(bin);
 
-      bin.bin = std::make_pair(1.131,1.305);
-      bin.name = "eta1113";
-      bin.title = "1.1 #leq |#eta| < 1.3";
+      bin.bin = std::make_pair(1.044,1.305);
+      bin.name = "eta1013";
+      bin.title = "1.044 #leq |#eta| < 1.305";
       mEtaBins.push_back(bin);
 
-      bin.bin = std::make_pair(1.305,1.740);
-      bin.name = "eta1317";
-      bin.title = "1.3 #leq |#eta| < 1.7";
+      bin.bin = std::make_pair(1.305,1.479);
+      bin.name = "eta1315";
+      bin.title = "1.305 #leq |#eta| < 1.479";
       mEtaBins.push_back(bin);
 
-      bin.bin = std::make_pair(1.740,2.043);
-      bin.name = "eta1720";
-      bin.title = "1.7 #leq |#eta| < 2.0";
+      bin.bin = std::make_pair(1.479,1.653);
+      bin.name = "eta1517";
+      bin.title = "1.479 #leq |#eta| < 1.653";
+      mEtaBins.push_back(bin);
+
+      bin.bin = std::make_pair(1.653,1.930);
+      bin.name = "eta1719";
+      bin.title = "1.653 #leq |#eta| < 1.930";
       mEtaBins.push_back(bin);
           
-      bin.bin = std::make_pair(2.043,2.322);
-      bin.name = "eta2023";
-      bin.title = "2.0 #leq |#eta| < 2.3";
+      bin.bin = std::make_pair(1.930,2.172);
+      bin.name = "eta1922";
+      bin.title = "1.930 #leq |#eta| < 2.172";
       mEtaBins.push_back(bin);
       
-      bin.bin = std::make_pair(2.322,2.853);
-      bin.name = "eta2329";
-      bin.title = "2.3 #leq |#eta| < 2.9";
+      bin.bin = std::make_pair(2.172,2.322);
+      bin.name = "eta2223";
+      bin.title = "2.172 #leq |#eta| < 2.322";
       mEtaBins.push_back(bin);
       
+      bin.bin = std::make_pair(2.322,2.500);
+      bin.name = "eta2325";
+      bin.title = "2.322 #leq |#eta| < 2.5";
+      mEtaBins.push_back(bin);
       
+      bin.bin = std::make_pair(2.500,2.650);
+      bin.name = "eta2527";
+      bin.title = "2.5 #leq |#eta| < 2.65";
+      mEtaBins.push_back(bin);
       
-      bin.bin = std::make_pair(2.853,5.191);
-      bin.name = "eta2951";
-      bin.title = "2.9 #leq |#eta| < 5.1";
+      bin.bin = std::make_pair(2.650,2.853);
+      bin.name = "eta2729";
+      bin.title = "2.65 #leq |#eta| < 2.853";
+      mEtaBins.push_back(bin);
+      
+      bin.bin = std::make_pair(2.853,2.964);
+      bin.name = "eta2930";
+      bin.title = "2.853 #leq |#eta| < 2.964";
+      mEtaBins.push_back(bin);
+      
+      bin.bin = std::make_pair(2.964,3.139);
+      bin.name = "eta3031";
+      bin.title = "2.964 #leq |#eta| < 3.319";
+      mEtaBins.push_back(bin);
+      
+      bin.bin = std::make_pair(3.139,3.489);
+      bin.name = "eta3135";
+      bin.title = "3.139 #leq |#eta| < 3.489";
+      mEtaBins.push_back(bin);
+      
+      bin.bin = std::make_pair(3.489,3.839);
+      bin.name = "eta3538";
+      bin.title = "3.489 #leq |#eta| < 3.839";
+      mEtaBins.push_back(bin);
+      
+      bin.bin = std::make_pair(3.839,5.191);
+      bin.name = "eta3852";
+      bin.title = "3.839 #leq |#eta| < 5.191";
       mEtaBins.push_back(bin);
       
      
