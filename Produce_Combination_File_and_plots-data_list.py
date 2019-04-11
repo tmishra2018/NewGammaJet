fb81e9583a32d90d64d257e9356abb66a8e25038
lucastorterotot <lucas.torterotot@gmail.com>
2019-04-11 16:49:25 +0200

    Only alpha = 0.3 for JER is relevant

--- Produce_Combination_File_and_plots-data_list.py
+++ Produce_Combination_File_and_plots-data_list.py
@@ -29,7 +29,7 @@ List_alpha = [List_alpha[0]]
 List_alpha_name = [List_alpha_name[0]]
 for alpha in List_alpha:
 	
-	cmd1 = "gammaJetFinalizer -i "+Input_data+" -d DATA_"+output+"_"+List_alpha_name[i_alpha]+"_"+str(today)+" --type pf --algo ak4  --runera "+Pu_profile+" --alpha "+str(alpha)
+	cmd1 = "gammaJetFinalizer --input-list "+Input_data+" -d DATA_"+output+"_"+List_alpha_name[i_alpha]+"_"+str(today)+" --type pf --algo ak4  --runera "+Pu_profile+" --alpha "+str(alpha)
 	dataname = "DATA_"+output+"_"+List_alpha_name[i_alpha]+"_"+str(today)+"_"+Pu_profile
 	os.system(cmd1)
 	
