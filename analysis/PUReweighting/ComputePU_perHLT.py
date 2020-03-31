cebc3d924a3727bddf82644cc52dbff59c65e223
lucastorterotot <lucas.torterotot@gmail.com>
2020-03-31 14:12:31 +0200

    Changes for 2017UL

--- analysis/PUReweighting/ComputePU_perHLT.py
+++ analysis/PUReweighting/ComputePU_perHLT.py
@@ -20,8 +20,7 @@ if not os.path.exists(Cert_json):
   
 print("\tDownloading the latest pileup file")
 cmd_1="rm pileup_latest.txt"
-#cmd_2="wget https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt"
-cmd_2="cp /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PileUp/pileup_latest.txt ."
+cmd_2="cp /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/PileUp/pileup_latest.txt ."
 print(cmd_2)
 
 os.system(cmd_1)
@@ -43,7 +42,7 @@ today.strftime('%d-%m-%Y')
 
 outputdirectory="PUfiles_"+Runsuffix+"_"+str(today)
 
-cmd="mkdir "+outputdirectory
+cmd="mkdir -p "+outputdirectory
 os.system(cmd)
 
 print("\tRunning brilcalc for each HLT")
@@ -59,7 +58,7 @@ for iHLT in HLT_list:
 	cmd2="pileupReCalc_HLTpaths.py -i "+outputdirectory+"/"+YourOutput+".csv --inputLumiJSON pileup_latest.txt -o "+outputdirectory+"/"+YourOutput+".txt --runperiod Run2"
 	print(cmd2)
 	os.system(cmd2)
-	outputROOT = "pu_truth_data2018_100bins_HLTphoton"+HLT_prefix_list[i_HLT]+Runsuffix+".root"
+	outputROOT = "pu_truth_data2017UL_100bins_HLTphoton"+HLT_prefix_list[i_HLT]+Runsuffix+".root"
 	print("\tRunning pileupCalc "+HLT_prefix_list[i_HLT])
 	cmd3= "pileupCalc.py -i "+Cert_json+" --inputLumiJSON "+outputdirectory+"/"+YourOutput+".txt --calcMode true --minBiasXsec 69200 --maxPileupBin 100 --numPileupBins 100 "+outputROOT
 	print(cmd3)
