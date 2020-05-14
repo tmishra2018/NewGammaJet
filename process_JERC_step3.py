98c6877ac8f525e0e275b69f7a8f1c1011f11fb7
lucastorterotot <lucas.torterotot@gmail.com>
2020-05-14 11:53:18 +0200

    Update process_JERC_step3.py according to JEC version

--- process_JERC_step3.py
+++ process_JERC_step3.py
@@ -3,6 +3,14 @@ import subprocess
 from functools import partial
 import multiprocessing as mp
 
+from optparse import OptionParser
+usage = "usage: %prog [options]"
+parser = OptionParser(usage=usage)
+parser.add_option("-d", "--dates", dest = "dates", default=None,
+                  help = "Give the dates of the Step2 outputs, by default it takes the most recent. Format is -d YYY-MM-DD,YYY-MM-DD,...")
+
+(options,args) = parser.parse_args()
+
 samples = {
     'A' : 'Run2018A-17Sep2018-v2',
     'B' : 'Run2018B-17Sep2018-v1',
@@ -11,12 +19,19 @@ samples = {
     'MC': 'GJet_Pt-15To6000_RunIIAutumn18MiniAOD-102X',
 }
 
-lumis_or_xsec_pb = {
-    'A' : 13654.355526985,
-    'B' : 7057.825158567,
-    'C' : 6894.770971269,
-    'D' : 31066.589629726,
-    'MC': 283000.0,
+merged_runs = ["ABC", "ABCD"]
+
+lumis_per_pb = {}
+# recomputed with brilcalc :
+lumis_per_pb = {
+    'Run2018A-17Sep2018-v2' : 13654.355526985,
+    'Run2018B-17Sep2018-v1' : 7057.825158567,
+    'Run2018C-17Sep2018-v1' : 6894.770971269,
+    'Run2018D-PromptReco-v2' : 31066.589629726,
+}
+
+xsec_pb = {
+    'GJet_Pt-15To6000_RunIIAutumn18MiniAOD-102X' : 283000.0,
 }
 
 JECs = ['wo_L2Res', 'only_L2Res', 'L2L3Res']
@@ -38,132 +53,185 @@ def print_log_started_step(run, JERC, step):
     command = "echo 'run "+run+' '+JERC+" "+step+" started' >> "+Step3_dir+'/tmp-process_logs.log'
     os.system(command)
 
-def wait_for(runs, JERC, step, time='1m'):
-    waiting = 0
-    for run in runs:
-        command = 'if cat '+Step3_dir+'/tmp-process_logs.log | grep -q '+"'run "+run+' '+JERC+" "+step+"'"
-        command += " ; then echo '0' ; else echo '1' ; fi"
-        waiting += int(os.popen(command).read()[:-1])
-    if not waiting == 0:
-        command = 'echo '+"'waiting "+time+" for runs "
-        for run in runs:
-            command += run+' '
-        command += JERC+" "+step+"'"
-        os.system(command)
-        os.system('sleep '+time)
-        wait_for(runs, JERC, step, time=time)
-
 def get_most_recent(directory):
     return os.popen('ls -t {}'.format(directory)).read().split('\n')[0]
 
 def find_files_from_step2(run, JERC):
     print_log_started_step(run, JERC, 'find files')
+
+    Step2_files_dir = "/".join([Step2_outputs_base_dir, samples[run]])
+    grep_path = "{}".format(JERC)
+
+    if options.dates is None:
+        Step2_files_dir = "/".join([Step2_files_dir, get_most_recent(Step2_files_dir)])
+    else:
+        grep_path += " | grep -e "+" -e ".join(options.dates.split(","))
+
     list_file = "Step2_files_{}_{}.txt".format(run,JERC)
-    command = 'cd '+Step3_scripts_dir
-    if run in samples.keys():
-        command +=' ; find '
-        directory = Step2_outputs_base_dir+'/'
-        directory += samples[run]+'/'
-        directory += get_most_recent(directory)+'/'
-        command += directory
-        command += ' -type f '
-        command += " -iname \*{}\*reduced_skim.root ".format(JERC)
-        command += " > "+list_file
-    if run in ['ABC', 'ABCD']:
-        wait_for([letter for letter in run], JERC, 'merged OK',time='10m')
-        command += ' ; rm -f '+list_file
-        for letter in run :
-            # find_files_from_step2(letter, JERC)
-            command += ' ; cat '+"Step2_files_{}_{}.txt".format(letter,JERC)
-            command += " >> "+list_file
+
+    command = "cd {Step3_scripts_dir} && find {Step2_files_dir} -type f -iname \*reduced_skim.root | grep {grep_path} > {list_file}".format(
+        Step3_scripts_dir = Step3_scripts_dir,
+        Step2_files_dir = Step2_files_dir,
+        grep_path = grep_path,
+        list_file = list_file,
+    )
     os.system(command)
         
 def merge_files_from_step2(run, JERC, cleaning=True):
     print_log_started_step(run, JERC, 'merge files')
-    command = 'cd '+Step3_scripts_dir
+
     output_dir = "/eos/user/${USER:0:1}/$USER/JEC-task/Step3_outputs/2018/"+JERC+'/'
-    os.system("mkdir -p {}".format(output_dir))
-    if run in lumis_or_xsec_pb.keys():
-        lumi_or_xsec_pb = lumis_or_xsec_pb[run]
-    elif run in ['ABC','ABCD']:
-        lumi_or_xsec_pb = 0
-        for letter in run:
-            lumi_or_xsec_pb += lumis_or_xsec_pb[letter]
-    if run == 'MC':
-        command += ' ; python mergeAndAddWeights.py -c {} '.format(cleaning)
-    else:
-        command += ' ; python mergeData.py -c {} '.format(cleaning)
-    command += " -i Step2_files_{}_{}.txt".format(run,JERC)
-    command += " -o "+output_dir
-    if run == 'MC':
-        command += " --xsec "
-    else:
-        command += " --lumi_tot "
-    command += str(lumi_or_xsec_pb)
-    if not run == 'MC':
-        command += " --run "+run
-        command += " && echo 'run "+run+' '+JERC+" merged OK' >> "+Step3_dir+'/tmp-process_logs.log'
+    command_mkdir = "mkdir -p {}".format(output_dir)
+
+    command_cd = 'cd {}'.format(Step3_scripts_dir)
+
+    input_list = "Step2_files_{}_{}.txt".format(run,JERC)
+
+    if samples[run] in lumis_per_pb.keys():
+        script = "mergeData.py"
+        extras = "--lumi_tot {} --run {}".format(lumis_per_pb[samples[run]], run)
+    elif samples[run] in xsec_pb.keys():
+        script = "mergeAndAddWeights.py"
+        extras = "--xsec {} --JEC {}".format(xsec_pb[samples[run]], "_".join([samples[run], JERC]))
+
+    command = " && ".join([
+        command_mkdir, command_cd,
+        "python {script} -i {input_list} -o {output_dir} {extras}".format(
+            script = script,
+            input_list = input_list,
+            output_dir = output_dir,
+            extras = extras,
+        )
+    ])
     print command
     os.system(command)
 
-def make_pileup(run,JERC):
+def make_pileup_data(run,JERC):
     print_log_started_step(run, JERC, 'make pileup')
-    command = 'cd '+Step3_PU_dir
-    if run == 'MC':
-        command += ' ; echo '+get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/*_MC_*')+' > files_2018_MC_'+JERC+'.list'
-        command += ' ; ./generate_mc_pileup.exe files_2018_MC_'+JERC
-    if not run in samples.keys():
-        processedLumis = ''
-        for letter in run :
-            processedLumis += os.popen('cat '+Step1_outputs_crab_dir+'/crab_'+samples[letter]+'/results/processedLumis.json').read()[:-1]
-        processedLumis = processedLumis.replace('}{',', ')
-        command += " ; echo '"+processedLumis+"' > ./tmp_processedLumis_"+run+'_'+JERC+'.json'
-    if not run == 'MC':
-        command += ' ; python ComputePU_perHLT.py'
-        if run in samples.keys():
-            command += ' --Cert_json '+Step1_outputs_crab_dir+'/crab_'+samples[run]+'/results/processedLumis.json'
-        else:
-            command += ' --Cert_json ./tmp_processedLumis_'+run+'_'+JERC+'.json'
-        command += ' --whichrun '+run+'_'+JERC
-    if run == 'MC':
-        command += " && echo 'run MC "+JERC+" PU OK' >> "+Step3_dir+'/tmp-process_logs.log'
+    cmds = []
+    cmds.append('cd {}'.format(Step3_PU_dir))
+
+    if samples[run] in lumis_per_pb.keys(): # data
+        cmds.append("python ComputePU_perHLT.py --Cert_json {Cert_json} --whichrun {whichrun}".format(
+            Cert_json = "{}/crab_{}/results/processedLumis.json".format(Step1_outputs_crab_dir, samples[run]),
+            whichrun = "_".join([run, JERC]),
+        )
+    )
+    command = " && ".join(cmds)
+    print command
+    os.system(command)
+
+def make_pileup_data_merged(run,JERC):
+    print_log_started_step(run, JERC, 'make pileup')
+    cmds = []
+    cmds.append('cd {}'.format(Step3_PU_dir))
+
+    processedLumis = ''
+    for letter in run :
+        processedLumis += os.popen('cat '+Step1_outputs_crab_dir+'/crab_'+samples[letter]+'/results/processedLumis.json').read()[:-1]
+    processedLumis = processedLumis.replace('}{',', ')
+    cmds.append("echo '{}' > ./tmp_processedLumis_{}_{}.json".format(processedLumis, run, JERC))
+    cmds.append(
+        "python ComputePU_perHLT.py --Cert_json {Cert_json} --whichrun {whichrun}".format(
+            Cert_json = "./tmp_processedLumis_{}_{}.json".format(run, JERC),
+            whichrun = "_".join([run, JERC])
+        )
+    )
+
+    command = " && ".join(cmds)
+    print command
+    os.system(command)
+
+def make_pileup_MC(JERC):
+    run = "MC"
+    print_log_started_step(run, JERC, 'make pileup')
+    files_list = "files_PUreweight_2018_{}_{}".format(run, JERC)
+    cmds = []
+    cmds.append('cd {}'.format(Step3_PU_dir))
+    cmds.append("rm -f {list}".format(list = files_list))
+    for mc_sample in xsec_pb.keys():
+        cmds.append(
+            "echo {merged_file} >> {list}.list".format(
+                merged_file = get_most_recent(
+                    "{}/{}/*_MC_{}*".format(Step3_outputs_base_dir, JERC, "_".join([mc_sample, JERC]))
+                    ),
+                list = files_list,
+            )
+        )
+    cmds.append(
+        "./generate_mc_pileup.exe {list}".format(list = files_list)
+    )
+    command = " && ".join(cmds)
     print command
     os.system(command)
 
 def Produce_Combination_File_and_plots(run,JERC):
     print_log_started_step(run, JERC, 'make plots')
-    if not run == 'MC':
-        wait_for(['MC'], JERC, 'PU OK')
-        os.system("cd {} ; ln -sf computed_mc_files_2018_MC_{}_pu_truth_100bins.root computed_mc_files_2018_MC_for_{}_{}_pu_truth_100bins.root".format(
-            Step3_PU_dir,
-            JERC,
-            run,
-            JERC
-            )
-        )
+
+    os.system("cd {} ; ln -sf computed_mc_files_PUreweight_2018_MC_{}_pu_truth_100bins.root computed_mc_files_2018_MC_for_{}_{}_pu_truth_100bins.root".format(
+        Step3_PU_dir,
+        JERC,
+        run,
+        JERC,
+    )
+    )
+
     output = JERC
     IOV = run+'_'+JERC
     Pu_profile = run+'_'+JERC
-    Input_data = get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/'+'PhotonJet_2ndLevel_DATA_RUN_'+run+'_*')
-    Input_mc = get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/'+'PhotonJet_2ndLevel_MC_*')
+    if len(run) == 1:
+        script = "Produce_Combination_File_and_plots-MC_list.py"
+        Input_data = get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/'+'PhotonJet_2ndLevel_DATA_RUN_'+run+'_*')
+    else:
+        script ="Produce_Combination_File_and_plots-DATA_and_MC_list.py"
+        Input_data = [get_most_recent("{}/{}/PhotonJet_2ndLevel_DATA_RUN_merged_{}_for_{}_*".format(Step3_outputs_base_dir,JERC,run[0],run))]
+        for r in run[1:]:
+            Input_data.append(get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/'+'PhotonJet_2ndLevel_DATA_RUN_'+r+'_*'))
+        os.system("cd {} && rm -f input_data_{}_{}.list".format(Step3_dir, run, JERC))
+        for file in Input_data:
+            os.system("cd {} && echo '{}' >> input_data_{}_{}.list".format(Step3_dir, file, run, JERC))
+        Input_data = '{}/input_data_{}_{}.list'.format(Step3_dir, run, JERC)
+    Input_mc = []
+    for mc_sample in [m for m in samples.keys() if 'MC' in m]:
+        Input_mc.append(get_most_recent(Step3_outputs_base_dir+'/'+JERC+'/'+'PhotonJet_2ndLevel_MC_{}*{}*'.format(samples[mc_sample], JERC)))
+    os.system("cd {} && rm -f input_mc_{}_{}.list".format(Step3_dir, run, JERC))
+    for file in Input_mc:
+        os.system("cd {} && echo '{}' >> input_mc_{}_{}.list".format(Step3_dir, file, run, JERC))
     command = 'cd '+Step3_dir
-    command += ' ; python Produce_Combination_File_and_plots.py'
+    command += ' ; python {}'.format(script)
     command += ' --output='+output
     command += ' --IOV='+IOV
     command += ' --Pu_profile='+Pu_profile
     command += ' --Input_data='+Input_data
-    command += ' --Input_mc='+Input_mc
+    command += ' --Input_mc='+'{}/input_mc_{}_{}.list'.format(Step3_dir, run, JERC)
+    print command
     os.system(command)
 
-def multithreadmap(process,X,ncores=8, **kwargs):
-    """
-    multithreading map of a function, default on 8 cpu cores.
-    """
-    func = partial(process, **kwargs)
-    p=mp.Pool(ncores)
-    Xout = p.map(func,X)
-    p.terminate()
-    return(Xout)
+def get_copy_for_merge(mrun, JERC):
+    run_to_copy = mrun[0]
+    command = "cp {} {}".format(
+        get_most_recent("{}/{}/PhotonJet_2ndLevel_DATA_RUN_{}_*".format(Step3_outputs_base_dir,JERC,run_to_copy)),
+        get_most_recent(
+            "{}/{}/PhotonJet_2ndLevel_DATA_RUN_{}_*".format(Step3_outputs_base_dir,JERC,run_to_copy)
+        ).replace("_RUN_{}_".format(run_to_copy), "_RUN_merged_{}_for_{}_".format(run_to_copy, mrun))
+    )
+    print(command)
+    os.system(command)
+
+def set_lumi_for_merge(mrun, JERC):
+    lumi = 0
+    for run in mrun:
+        lumi += lumis_per_pb[samples[run]]
+    file = get_most_recent("{}/{}/PhotonJet_2ndLevel_DATA_RUN_merged_*_for_{}_*".format(Step3_outputs_base_dir,JERC,mrun))
+    set_lumi(file, lumi)
+
+def set_lumi(file, lumi):
+    from ROOT import TFile
+    tfile = TFile(file, "UPDATE")
+    _lumi = tfile.Get("totallumi")
+    _lumi.SetVal(lumi)
+    _lumi.Write()
+    tfile.Close()
 
 ##############################################################################
 ##############################################################################
@@ -184,42 +252,64 @@ elif CMSSW_BASE == Step3_BASE_JER:
 else:
     print "Ouch, the directories are not as they are supposed to be. Did you followed the git installation instructions correctly?"
 
-
-def process(run_JERC):
-    run, JERC = run_JERC
-    find_files_from_step2(run, JERC)
-    merge_files_from_step2(run, JERC)
-    
-    make_pileup(run,JERC)
-    
-    if not run == 'MC' :
-        Produce_Combination_File_and_plots(run,JERC)
-
 os.system('rm -f '+Step3_dir+'/tmp-process_logs.log')
 os.system('touch '+Step3_dir+'/tmp-process_logs.log')
 
+run_JERCs_MC = []
+run_JERCs_data = []
+MC_pileup_JERCs = set()
 
-done_run_JERCs = []
-# for run in ['A','B', 'MC']:
-#     for JERC in JERCs:
-#         done_run_JERCs.append((run,JERC))
-# for run in ['C']:
-#     for JERC in ['wo_L2Res', 'L2L3Res', 'JER']:
-#         done_run_JERCs.append((run,JERC))
-# for run in ['D']:
-#     for JERC in ['wo_L2Res', 'L2L3Res', 'only_L2Res']:
-#         done_run_JERCs.append((run,JERC))
+for JERC in JERCs:
+    for run in samples.keys():
+        if samples[run] in lumis_per_pb.keys():
+            run_JERCs_data.append((run, JERC))
+        elif samples[run] in xsec_pb.keys():
+            run_JERCs_MC.append((run, JERC))
 
-run_JERCs = []
-sorted_runs_for_multithreadmap = ['MC', 'C', 'B']
-sorted_runs_for_multithreadmap = [run for run in sorted_runs_for_multithreadmap if run in samples.keys()]
-sorted_runs_for_multithreadmap += [key for key in samples.keys() if not key in sorted_runs_for_multithreadmap]
-sorted_runs_for_multithreadmap += ['ABC', 'ABCD']
-for run in sorted_runs_for_multithreadmap:
-    for JERC in JERCs:
-        if not (run,JERC) in done_run_JERCs:
-                run_JERCs.append((run,JERC))
+run_JERCs = run_JERCs_MC + run_JERCs_data
+
 print run_JERCs
+
 os.system("sleep 10s")
-multithreadmap(process, run_JERCs)
+for run_JERC in run_JERCs_MC:
+    if run_JERC in run_JERCs:
+        run, JERC = run_JERC
+        find_files_from_step2(run, JERC)
+        merge_files_from_step2(run, JERC)
+        MC_pileup_JERCs.add(JERC)
+
+for MC_pileup_JERC in MC_pileup_JERCs:
+    make_pileup_MC(MC_pileup_JERC)
+
+for run_JERC in run_JERCs_data:
+    if run_JERC in run_JERCs:
+        run, JERC = run_JERC
+
+        find_files_from_step2(run, JERC)
+        merge_files_from_step2(run, JERC)
+
+        make_pileup_data(run,JERC)
+
+        Produce_Combination_File_and_plots(run,JERC)
+
+for merged_run in merged_runs:
+    done_jercs = {}
+    for mrun in merged_run:
+        done_jercs[mrun] = set()
+        for run_JERC in run_JERCs:
+            run, JERC = run_JERC
+            if mrun == run:
+                done_jercs[mrun].add(JERC)
+    todo_jercs = set([jerc for jerc in done_jercs[mrun]])
+    for mrun in merged_run:
+        todo_jercs = todo_jercs.intersection(done_jercs[mrun])
+
+    for JERC in todo_jercs:
+        get_copy_for_merge(merged_run, JERC)
+        set_lumi_for_merge(merged_run, JERC)
+
+        make_pileup_data_merged(merged_run,JERC)
+
+        Produce_Combination_File_and_plots(merged_run,JERC)
+
 os.system('rm -f '+Step3_dir+'/tmp-process_logs.log')
